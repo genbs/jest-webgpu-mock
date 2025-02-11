6570927be69dbff1d98d101824bfad1d65e39011
genbs <gennaro.bs@gmail.com>
2025-02-11 17:13:10 +0100

    add mock

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,4 @@
-import { MockGPUDevice, mockWebGPU } from "../lib/index"
+import { MockGPUAdapter, MockGPUDevice, mockWebGPU } from "../lib/index"
 
 describe("WebGPU Mock", () => {
 	it("should setup after resetAllMocks", () => {
@@ -12,7 +12,7 @@ describe("WebGPU Mock", () => {
 
 	it("should mock GPUDevice", async () => {
 		const adapter = await navigator.gpu.requestAdapter()
-		expect(adapter).toBeInstanceOf(MockGPUDevice)
+		expect(adapter).toBeInstanceOf(MockGPUAdapter)
 		expect(navigator.gpu.getPreferredCanvasFormat()).toBe("bgra8unorm")
 	})
 
@@ -20,5 +20,46 @@ describe("WebGPU Mock", () => {
 		const device = new MockGPUDevice()
 		const buffer = device.createBuffer()
 		expect(buffer).toBeDefined()
+	})
+
+	it("should mock createBindGroupLayout", async () => {
+		const device = new MockGPUDevice()
+		const bindGroupLayout = device.createBindGroupLayout()
+		expect(bindGroupLayout).toBeDefined()
+	})
+
+	it("should mock createBindGroup", async () => {
+		const device = new MockGPUDevice()
+		const bindGroup = device.createBindGroup()
+		expect(bindGroup).toBeDefined()
+	})
+
+	it("should mock GPURenderPassEncoder setBindGroup", async () => {
+		const device = new MockGPUDevice()
+		const commandEncoder = device.createCommandEncoder()
+		const passEncoder = commandEncoder.beginRenderPass()
+		expect(passEncoder.setBindGroup).toBeDefined()
+		expect(passEncoder.end).toBeDefined()
+		const bindGroup = device.createBindGroup()
+		passEncoder.setBindGroup(0, bindGroup)
+		expect(passEncoder).toBeDefined()
+	})
+
+	it("should mock device queue submit", async () => {
+		const device = new MockGPUDevice()
+		const commandEncoder = device.createCommandEncoder()
+		const passEncoder = commandEncoder.beginRenderPass()
+		passEncoder.end()
+		const commandBuffer = commandEncoder.finish()
+		device.queue.submit([commandBuffer])
+		expect(device.queue.submit).toBeDefined()
+	})
+
+	it("should mock device queue writeBuffer", async () => {
+		const device = new MockGPUDevice()
+		const buffer = device.createBuffer()
+		const data = new ArrayBuffer(8)
+		device.queue.writeBuffer(buffer, 0, data)
+		expect(device.queue.writeBuffer).toBeDefined()
 	})
 })
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { mockWebGPUCanvas } from "./canvas"
 
 export class MockGPUCommandEncoder {
-	beginRenderPass() {}
+	beginRenderPass() {
+		return new MockGPURenderPassEncoder()
+	}
 	endRenderPass() {}
+	finish() {}
 }
 
 export class MockGPUDevice {
@@ -10,21 +13,35 @@ export class MockGPUDevice {
 		return new MockGPUCommandEncoder()
 	}
 
-	createSampler() {
-		return {}
-	}
-}
-
-export class MockGPUAdapter {
-	requestDevice(descriptor?: GPUDeviceDescriptor) {
-		return Promise.resolve(new MockGPUDevice())
-	}
 	createBuffer() {
 		return new MockGPUBuffer()
 	}
 
 	createTexture() {
 		return new MockGPUTexture()
+	}
+
+	createSampler() {
+		return new MockGPUSampler()
+	}
+
+	createBindGroupLayout() {
+		return new MockGPUBindGroupLayout()
+	}
+
+	createBindGroup() {
+		return new MockGPUBindGroup()
+	}
+
+	queue = {
+		submit: (commandBuffers: MockGPUCommandEncoder[]) => {},
+		writeBuffer: (buffer: MockGPUBuffer, offset: number, data: ArrayBuffer) => {},
+	}
+}
+
+export class MockGPUAdapter {
+	requestDevice() {
+		return Promise.resolve(new MockGPUDevice())
 	}
 }
 
@@ -34,7 +51,22 @@ export class MockGPUBuffer {
 	}
 }
 
-export class MockGPUTexture {}
+export class MockGPUTexture {
+	createView() {
+		return new MockGPUTextureView()
+	}
+}
+export class MockGPUTextureView {}
+export class MockGPUSampler {}
+export class MockGPUBindGroupLayout {}
+export class MockGPUBindGroup {}
+
+export class MockGPURenderPassEncoder {
+	setPipeline() {}
+	draw() {}
+	end() {}
+	setBindGroup(index: number, bindGroup: MockGPUBindGroup) {}
+}
 
 const mockGPU = {
 	requestAdapter: async () => new MockGPUAdapter(),
@@ -45,6 +77,61 @@ const mockGPU = {
 
 export function mockWebGPU() {
 	globalThis.navigator.gpu = mockGPU
+
+	if (!globalThis.GPUTexture) {
+		// @ts-ignore
+		globalThis.GPUTexture = MockGPUTexture
+	}
+	if (!globalThis.GPUTextureUsage) {
+		// @ts-ignore
+		globalThis.GPUTextureUsage = {
+			COPY_SRC: 1,
+			COPY_DST: 2,
+			TEXTURE_BINDING: 3,
+			STORAGE_BINDING: 4,
+			RENDER_ATTACHMENT: 5,
+		}
+	}
+
+	if (!globalThis.GPUBufferUsage) {
+		// @ts-ignore
+		globalThis.GPUBufferUsage = {
+			MAP_READ: 1,
+			MAP_WRITE: 2,
+			COPY_SRC: 4,
+			COPY_DST: 8,
+			INDEX: 16,
+			VERTEX: 32,
+			UNIFORM: 64,
+			STORAGE: 128,
+			INDIRECT: 256,
+			QUERY_RESOLVE: 512,
+		}
+	}
+
+	if (!globalThis.GPUShaderStage) {
+		// @ts-ignore
+		globalThis.GPUShaderStage = {
+			VERTEX: 1,
+			FRAGMENT: 2,
+			COMPUTE: 4,
+		}
+	}
+
+	if (!globalThis.GPUTextureView) {
+		// @ts-ignore
+		globalThis.GPUTextureView = MockGPUTextureView
+	}
+
+	if (!globalThis.GPUSampler) {
+		// @ts-ignore
+		globalThis.GPUSampler = MockGPUSampler
+	}
+
+	// globalThis.GPUBuffer = MockGPUBuffer
+	// globalThis.GPUDevice = MockGPUDevice
+	// globalThis.GPUAdapter = MockGPUAdapter
+	// globalThis.GPUCommandEncoder = MockGPUCommandEncoder
 
 	mockWebGPUCanvas()
 }
